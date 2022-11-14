01347bf408eebbba6fc9d79e248a3cee7b1d8907
Rin <82919982+Rin-chan@users.noreply.github.com>
2022-11-14 16:20:39 +0800

    Make Rainbow unstackable

--- src/main/java/github/Rin_Chan/Punmod/item/RainbowItem.java
+++ src/main/java/github/Rin_Chan/Punmod/item/RainbowItem.java
@@ -4,6 +4,6 @@ import net.minecraft.world.item.Item;
 
 public class RainbowItem extends Item {
 	public RainbowItem() {
-		super(new Item.Properties().tab(ModItemGroup.PUN_MOD));
+		super(new Item.Properties().stacksTo(1).tab(ModItemGroup.PUN_MOD));
 	}
 }
