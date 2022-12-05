3763fccaa8da62b9bd06343afd2cc7e4c7b0987a
Rin <82919982+Rin-chan@users.noreply.github.com>
2022-12-05 15:55:17 +0800

    Minor changes to stats

--- src/main/java/github/Rin_Chan/Punmod/item/PunisherItem.java
+++ src/main/java/github/Rin_Chan/Punmod/item/PunisherItem.java
@@ -1,12 +1,19 @@
 package github.Rin_Chan.Punmod.item;
 
 import github.Rin_Chan.Punmod.util.ModItemGroup;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.FireworkRocketEntity;
+import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tiers;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 
 public class PunisherItem extends SwordItem {
 	public PunisherItem() {
-		super(Tiers.DIAMOND, 3, -2.4F, (new Item.Properties()).tab(ModItemGroup.PUN_MOD));
+		super(Tiers.NETHERITE, 3, -1F, (new Item.Properties()).tab(ModItemGroup.PUN_MOD));
 	}
 }
