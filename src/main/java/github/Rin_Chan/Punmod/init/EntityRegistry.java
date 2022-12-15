6200dc2d8215a0632b5d2e4c446922c72086caa9
Rin <82919982+Rin-chan@users.noreply.github.com>
2022-12-15 09:36:42 +0800

    Remove unused imports

--- src/main/java/github/Rin_Chan/Punmod/init/EntityRegistry.java
+++ src/main/java/github/Rin_Chan/Punmod/init/EntityRegistry.java
@@ -2,7 +2,6 @@ package github.Rin_Chan.Punmod.init;
 
 import github.Rin_Chan.Punmod.PunMod;
 import github.Rin_Chan.Punmod.entity.DropletArrowEntity;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -14,8 +13,8 @@ public class EntityRegistry {
 	public static DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITIES, PunMod.MODID);
 	
 	public static final RegistryObject<EntityType<DropletArrowEntity>> DROPLET_ARROW = ENTITY_TYPES.register("droplet_arrow", () -> EntityType.Builder.of((EntityType.EntityFactory<DropletArrowEntity>) DropletArrowEntity::new, MobCategory.MISC).sized(0.5F, 0.5F).build("droplet_arrow"));
-	
-    public static void init() {
+
+	public static void init() {
     	ENTITY_TYPES.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 }
